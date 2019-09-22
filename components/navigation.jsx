34bcd69409cf91d3ec4a88f85bdfd5bc903adf4e
Dave Cole <dave@wistia.com>
2019-09-22 15:24:15 -0400

    Mobile mode for nav, with hambuger toggler button

--- components/navigation.jsx
+++ components/navigation.jsx
@@ -1,6 +1,9 @@
-import Link from "next/link";
+import Link from 'next/link';
+import { useState } from 'react';
 
 export default () => {
+  const [navMenuIsOpen, setNavMenuIsOpen] = useState(false);
+
   return (
     <>
       <style jsx>{`
@@ -9,6 +12,7 @@ export default () => {
           margin: 1em auto;
           display: block;
         }
+
         nav {
           display: flex;
           align-items: center;
@@ -17,6 +21,7 @@ export default () => {
           padding: 1.5em 0;
           background: rgb(197, 214, 204);
         }
+
         a {
           color: #303030;
           text-decoration: none;
@@ -29,8 +34,73 @@ export default () => {
         a:hover {
           border-bottom: 1px solid #303030;
         }
+
+        .nav-toggler-button {
+          background: transparent;
+          border: none;
+          display: none;
+          height: 60px;
+          position: absolute;
+          right: 0;
+          top: 0;
+          width: 60px;
+          z-index: 4;
+        }
+
+        .nav-toggler-icon-line {
+          background-color: #303030;
+          border-radius: 1.5px;
+          height: 3px;
+          left: 50%;
+          position: absolute;
+          top: 50%;
+          transition: all 250ms;
+          width: 50%;
+        }
+
+        .nav-toggler-icon-top-line {
+          transform: translateX(-50%) translateY(${navMenuIsOpen ? "0px" : "-10px"}) rotate(${navMenuIsOpen ? "-45deg" : "0deg"});
+        }
+
+        .nav-toggler-icon-middle-line {
+          opacity: ${navMenuIsOpen ? 0 : 1};
+          transform: translateX(-50%) translateY(0px) rotate(${navMenuIsOpen ? "-45deg" : "0deg"});
+        }
+
+        .nav-toggler-icon-bottom-line {
+          transform: translateX(-50%) translateY(${navMenuIsOpen ? "0px" : "10px"}) rotate(${navMenuIsOpen ? "45deg" : "0deg"});
+        }
+
+
+        @media (max-width: 850px) {
+          nav {
+            flex-direction: column;
+            margin: 0;
+            padding: ${navMenuIsOpen ? '.5em 0' : '0'};
+            max-height: ${navMenuIsOpen ? '250px' : '0px'};
+            overflow: hidden;
+            transition: max-height 400ms, opacity 400ms;
+            opacity: ${navMenuIsOpen ? 1 : 0};
+          }
+
+          a {
+            margin: .5em 0;
+          }
+
+          .nav-toggler-button {
+            display: block;
+          }
+        }
       `}</style>
       <img className="floral-header" src="/static/floral-header.png"/>
+      <button
+        className="nav-toggler-button"
+        onClick={() => setNavMenuIsOpen(!navMenuIsOpen)}
+      >
+        <div className="nav-toggler-icon-line nav-toggler-icon-top-line"></div>
+        <div className="nav-toggler-icon-line nav-toggler-icon-middle-line"></div>
+        <div className="nav-toggler-icon-line nav-toggler-icon-bottom-line"></div>
+      </button>
       <nav>
         <Link href="/our-story">
           <a>Our Story</a>
